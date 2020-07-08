2ab0f3b8ab0c713a69152be74911b0044e9208a4
unknown <geos191@gmail.com>
2020-07-07 23:11:11 -0400

    nuevos js

---
+++ js/bgParallax.js
@@ -0,0 +1,46 @@
+(function (){
+
+	//Objeto con Propiedades de Parallax
+
+let propParallax = {
+
+	seccion: document.querySelector('.parallax'),
+	recorrido: null,
+	limite: null,
+
+}
+
+
+//Objeto con Metodos de Parallax
+
+let metParallax = {
+
+	inicio: function () {
+
+		window.addEventListener('scroll', metParallax.scrollParallax);
+	},
+
+	scrollParallax: function () {
+
+		propParallax.recorrido = window.pageYOffset;
+		propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;	
+		
+		if (propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite){
+
+			propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px';
+		} else {
+			propParallax.seccion.style.backgroundPositionY = 0;
+		}
+
+	}
+	
+}
+
+metParallax.inicio();
+
+//.pageYOffset indica el recorrido del scroll
+//propParallax.seccion.offsetTop tindica el espacio en px entre el tope del elemento y el inicio de la pag
+//propParallax.seccion.offsetHeight altura del elemento
+
+}())
+
---
+++ js/formulario.js
@@ -0,0 +1,17 @@
+
+
+//Objeto con Propiedades de Formulario
+
+let propFormulario = {
+
+
+
+}
+
+//Objeto con Metodos de Formulario
+
+let metFormulario = {
+
+
+
+}
